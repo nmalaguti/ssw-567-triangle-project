e24c25e7564b0465cc1031cb6beaea0ecdd64547
Nick Malaguti <nmalaguti@gmail.com>
2009-01-17 16:12:33 -0500

    made each check a function. a triangle can now be more than one type of triangle

--- src/triangle.py
+++ src/triangle.py
@@ -1,24 +1,43 @@
 import sys
 
 def main(t):
+    # turn the strings from input to ints
     for i,j in enumerate(t):
         t[i] = int(j);
-    if(t[0] + t[1] <= t[2] 
-       or t[1] + t[2] <= t[0]
-       or t[2] + t[0] <= t[1]):
+    # make sure it's a triangle
+    if isNotTriangle(t):
         print("Invalid Triangle");
-    elif(t[0] == t[1] == t[2]):
-        print("Equilateral Triangle");
-    elif(t[0] != t[1] != t[2]):
-        print("Scalene Triangle");
-    elif(t[0] == t[1] != t[2]
-         or t[1] == t[2] != t[0]
-         or t[2] == t[0] != t[1]):
-        print("Isosceles Triangle");
-    elif(t[0]**2 + t[1]**2 == t[2]**2
-       or t[1]**2 + t[2]**2 == t[0]**2
-       or t[2]**2 + t[0]**2 == t[1]**2):
-        print("Right Triangle");
+    else:
+        # a triangle can be a right triangle as well as others
+        if isRight(t):
+            print("Right Triangle");
+        if isEquilateral(t):
+            print("Equilateral Triangle");
+        if isScalene(t):
+            print("Scalene Triangle");
+        if isIsosceles(t):
+            print("Isosceles Triangle");
 
+def isNotTriangle(t):
+    return(t[0] + t[1] <= t[2]
+           or t[1] + t[2] <= t[0]
+           or t[2] + t[0] <= t[1]);
+
+def isEquilateral(t):
+    return(t[0] == t[1] == t[2])
+
+def isScalene(t):
+    return(t[0] != t[1] != t[2]);
+
+def isIsosceles(t):
+    return(t[0] == t[1] != t[2]
+           or t[1] == t[2] != t[0]
+           or t[2] == t[0] != t[1]);
+
+def isRight(t):
+    return(t[0]**2 + t[1]**2 == t[2]**2
+           or t[1]**2 + t[2]**2 == t[0]**2
+           or t[2]**2 + t[0]**2 == t[1]**2);
+    
 if __name__ == "__main__":
     main(sys.argv[1:]);
