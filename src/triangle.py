3a4fa9ad82cb7f823c3e449a6aadbed08590b6e5
Nick Malaguti <nmalaguti@gmail.com>
2009-01-17 16:34:00 -0500

    added comments. cleaned code. made sure there are only 3 command line args allowed. [#3 state:resolved]

--- src/triangle.py
+++ src/triangle.py
@@ -10,7 +10,6 @@ def main(t):
     # catch text inputs
     except ValueError as e:
         usage();
-        sys.exit(1);
     # make sure it's a triangle
     if isNotTriangle(t):
         print("Invalid Triangle");
@@ -26,28 +25,40 @@ def main(t):
             print("Isosceles Triangle");
 
 def isNotTriangle(t):
+    # if the length of 2 sides are less than or equal
+    # to the third side, then it is an impossible triangle
     return(t[0] + t[1] <= t[2]
            or t[1] + t[2] <= t[0]
            or t[2] + t[0] <= t[1]);
 
 def isEquilateral(t):
-    return(t[0] == t[1] == t[2])
+    # all sides are of equal length
+    return(t[0] == t[1] == t[2]);
 
 def isScalene(t):
+    # no sides are of equal length
     return(t[0] != t[1] != t[2]);
 
 def isIsosceles(t):
+    # exactly 2 sides are of equal length
     return(t[0] == t[1] != t[2]
            or t[1] == t[2] != t[0]
            or t[2] == t[0] != t[1]);
 
 def isRight(t):
+    # one of the internal angles of the triangle is 90 degrees
     return(t[0]**2 + t[1]**2 == t[2]**2
            or t[1]**2 + t[2]**2 == t[0]**2
            or t[2]**2 + t[0]**2 == t[1]**2);
            
 def usage():
+    # prints the program usage and exits
     print("usage: ./triangle.py a b c\na, b, and c must be integers");
+    sys.exit(1);
     
 if __name__ == "__main__":
-    main(sys.argv[1:]);
+    # ensure 3 command line arguments
+    if(4 != len(sys.argv)):
+        usage();
+    else:
+        main(sys.argv[1:]);
